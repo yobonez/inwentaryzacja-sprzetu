ce4bbef7b34a38b63a6ae369333ff25fd495a035
Jakub Początek <136334753+Rorschach-IT@users.noreply.github.com>
2025-01-10 01:25:29 +0100

    tytuł do pliku .pdf przy generowaniu raportów stałych

--- website/changesHistory/scripts/main.js
+++ website/changesHistory/scripts/main.js
@@ -1,10 +1,6 @@
 // main.js
 import * as functions from "./functions.js";
 
-window.onload = () => {
-    document.title = "Historia Zmian w systemie inwentaryzacja " + reverseDateFormat();
-};
-
 document.getElementById("first-change-date").innerHTML = functions.displayCurrentDate("date");
 document.getElementById("first-change-time").innerHTML = functions.displayCurrentDate("time");
 
@@ -14,5 +10,4 @@ document.getElementById("second-change-time").innerHTML = functions.displayProvi
 document.getElementById("third-change-date").innerHTML = functions.displayProvidedDate('2024-12-31 15:43:09',"date");
 document.getElementById("third-change-time").innerHTML = functions.displayProvidedDate('2024-12-30 15:43:09', "time");
 
-// Set the dynamic title
-document.title = "Historia Zmian w systemie inwentaryzacja " + functions.reverseDateFormat()
+document.title = "Historia Zmian w systemie inwentaryzacja " + functions.reverseDateFormat();
